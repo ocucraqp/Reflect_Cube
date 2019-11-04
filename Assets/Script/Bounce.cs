6777743a68362599d5c34cdb9bf3ba58ed04e91a
大浦雅也 <masaya@oouramasayanoMacBook-Pro.local>
2019-11-04 13:44:36 +0900

    adjust ball reflection

---
+++ Assets/Script/Bounce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    private Vector3 lastVelocity;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = this.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        this.rb.velocity *= 0.995f;
+        this.lastVelocity = this.rb.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 refrectVec = Vector3.Reflect(this.lastVelocity, collision.contacts[0].normal);
+        this.rb.velocity = refrectVec;
+    }
+}
