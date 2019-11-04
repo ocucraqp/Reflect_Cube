79146b9b4d26e952ec9573d35e375eb8004d81bb
Masapyon <mairu0514m.o@gmail,com>
2019-11-04 21:56:27 +0900

    set Ball HP

--- Assets/Script/Ball_HP.cs
+++ Assets/Script/Ball_HP.cs
@@ -4,31 +4,24 @@ using UnityEngine;
 
 public class Ball_HP : MonoBehaviour
 {
-    public const int MAX_BALL = 20;
+    //public const int MAX_BALL = 20;
 
-    private int[] ball_hp = new int[MAX_BALL];
-    private GameObject[] ball = new GameObject[MAX_BALL];
+    //private int[] ball_hp = new int[MAX_BALL];
+    //private GameObject[] ball = new GameObject[MAX_BALL];
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        ball[0] = GameObject.Find("Ball11");
-        for (int i = 0; i < MAX_BALL; i++)
-        {
-            ball_hp[i] = i + 1;
-        }
-    }
+    [SerializeField]
+    private int ball_hp;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Plane" || collision.gameObject.name == "Plane (1)"
-            || collision.gameObject.name == "Plane (2)" || collision.gameObject.name == "Plane (3)"
-            || collision.gameObject.name == "Plane (4)" || collision.gameObject.name == "Plane (5)")
+        if (collision.gameObject.name == "Wall1" || collision.gameObject.name == "Wall2"
+            || collision.gameObject.name == "Wall3" || collision.gameObject.name == "Wall4"
+            || collision.gameObject.name == "Wall5" || collision.gameObject.name == "Wall6)")
         {
-            ball_hp[0]--;
-            if (ball_hp[0] <= 0)
+            ball_hp--;
+            if (ball_hp <= 0)
             {
-                Destroy(ball[0].gameObject);
+                Destroy(this.gameObject);
             }
         }
     }
