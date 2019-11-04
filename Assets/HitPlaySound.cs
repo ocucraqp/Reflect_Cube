5696ca5cea0e899505754d1b59a3cf725997cdea
kbys <riotdo31@gmail.com>
2019-11-04 23:26:54 +0900

    Add collision sounds.

---
+++ Assets/HitPlaySound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AudioClip sound;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
