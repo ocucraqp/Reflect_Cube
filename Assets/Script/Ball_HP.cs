560b47fd44cbe1fd280b6dc4d17c62370b0d8afd
Masapyon <mairu0514m.o@gmail,com>
2019-11-04 18:33:36 +0900

    add game management script

---
+++ Assets/Script/Ball_HP.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_HP : MonoBehaviour
+{
+    public const int MAX_BALL = 20;
+
+    private int[] ball_hp = new int[MAX_BALL];
+    private GameObject[] ball = new GameObject[MAX_BALL];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball[0] = GameObject.Find("Ball11");
+        for (int i = 0; i < MAX_BALL; i++)
+        {
+            ball_hp[i] = i + 1;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Plane" || collision.gameObject.name == "Plane (1)"
+            || collision.gameObject.name == "Plane (2)" || collision.gameObject.name == "Plane (3)"
+            || collision.gameObject.name == "Plane (4)" || collision.gameObject.name == "Plane (5)")
+        {
+            ball_hp[0]--;
+            if (ball_hp[0] <= 0)
+            {
+                Destroy(ball[0].gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public const int player_count = 2;
+
+    private int[] point = new int[player_count];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < player_count; i++)
+        {
+            point[i] = 0;
+        }
+    }
+
+    public void point_counter()
+    {
+
+    }
+}
