9badb53e9cdde6467eba7b0850e8ad32cf7bf533
lanqiu <ksumebosi@gmail.com>
2019-11-05 14:59:37 +0900

    add cube's material, que's material, wireframe script

---
+++ Assets/Script/Wireframe.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wireframe : MonoBehaviour
+{
+    Mesh mesh;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+
+        var triangleList = mesh.GetIndices(0);
+        var triangleNum = triangleList.Length / 3;
+        var lineList = new int[triangleNum * 6];
+        for (var n = 0; n < triangleNum ; ++n)
+        {
+            var t = n * 3;
+            var l = n * 6;
+            lineList[l+0] = triangleList[t+0];
+            lineList[l+1] = triangleList[t+1];
+
+            lineList[l+2] = triangleList[t+1];
+            lineList[l+3] = triangleList[t+2];
+
+            lineList[l+4] = triangleList[t+2];
+            lineList[l+5] = triangleList[t+0];
+        }
+        mesh.SetIndices(lineList, MeshTopology.Lines, 0);
+
+    }
+
+}
